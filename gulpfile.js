8b7f4da73feb982bd35b65c418a861f26e1452d1
xrr2016 <13286904669@163.com>
2016-11-14 08:48:53 +0800

    start project

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp')
+const imagemin = require('gulp-imagemin')
+
+gulp.task('imgMin',function () {
+  return gulp.src('./public/images/*')
+             .pipe(imagemin())
+             .pipe(gulp.dest('./dist/images/'))
+})
+
+gulp.task('default',function () {
+  gulp.start(['imgMin'])
+})
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
+//首页
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' })
 });
 
-module.exports = router;
+
+
+module.exports = router
