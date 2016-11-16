e970d28f80908d88361a4ed530a967fb190dea50
xrr2016 <13286904669@163.com>
2016-11-16 08:24:13 +0800

    add router all

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,54 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+
+const navbar = {
+    index: '首页',
+    products: '我们的产品',
+    service: '我们的服务',
+    about: '关于我们',
+    marketing: '营销网络',
+    career: '职业发展',
+    contact: '联系我们'
+}
+
+router.use(function(req, res, nexr) {
+    res.send({
+        navabr: navbar
+    })
+})
+
 
 //首页
 router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: '唐图文创',
-        navbar:{
-          index:'首页',
-          products:'我们的产品',
-          service:'我们的服务',
-          about:'关于我们',
-          marketing:'营销网络',
-          career:'职业发展',
-          contact:'联系我们'
-        }
+        res.render('index', {})
     })
-});
-// 产品页面
+    // 产品页面
 router.get('/products', function(req, res, next) {
-  res.render('pages/products',{})
-})
-// 服务页面
+        res.render('pages/products', {})
+    })
+    // 服务页面
 router.get('/service', function(req, res, next) {
-  res.render('pages/service',{})
-})
-// 关于我们
+        res.render('pages/service', {})
+    })
+    // 关于我们
 router.get('/about', function(req, res, next) {
-  res.render('pages/about',{})
-})
-// 营销网络
+        res.render('pages/about', {})
+    })
+    // 营销网络
 router.get('/marketing', function(req, res, next) {
-  res.render('pages/marketing',{})
-})
-// 职业发展
+        res.render('pages/marketing', {})
+    })
+    // 职业发展
 router.get('/career', function(req, res, next) {
-  res.render('pages/career',{})
-})
-// 联系我们
+        res.render('pages/career', {})
+    })
+    // 联系我们
 router.get('/contact', function(req, res, next) {
-  res.render('pages/contact',{})
-})
-// 产品个例
+        res.render('pages/contact', {})
+    })
+    // 产品个例
 router.get('/products/:id', function(req, res, next) {
-  let id = req.params.id
+    let id = req.params.id
 
 })
 
