71031110dea8661e33a53f67ed214b783f2ccaea
xrr2016 <13286904669@163.com>
2016-11-17 11:48:57 +0800

    fix logo

--- routes/index.js
+++ routes/index.js
@@ -21,52 +21,60 @@ const products = {
 router.get('/', function(req, res, next) {
         res.render('index', {
             navbar: navbar,
-            isIndex:true
+            isIndex:true,
+            logo:false
         })
     })
     // 产品页面
 router.get('/products', function(req, res, next) {
         res.render('pages/products', {
-            navbar: navbar
+            navbar: navbar,
+            logo:true
         })
     })
     // 服务页面
 router.get('/service', function(req, res, next) {
         res.render('pages/service', {
             navbar: navbar,
-            products:products
+            products:products,
+            logo:true
         })
     })
     // 关于我们
 router.get('/about', function(req, res, next) {
         res.render('pages/about', {
             navbar: navbar,
-            isAbout:true
+            isAbout:true,
+            logo:true
         })
     })
     // 营销网络
 router.get('/marketing', function(req, res, next) {
         res.render('pages/marketing', {
-            navbar: navbar
+            navbar: navbar,
+            logo:true
         })
     })
     // 职业发展
 router.get('/career', function(req, res, next) {
         res.render('pages/contact', {
-            navbar: navbar
+            navbar: navbar,
+            logo:true
         })
     })
     // 联系我们
 router.get('/contact', function(req, res, next) {
         res.render('pages/contact', {
-            navbar: navbar
+            navbar: navbar,
+            logo:true
         })
     })
     // 产品个例
 router.get('/products/:id', function(req, res, next) {
     let id = req.params.id
     res.render('product', {
-        navbar: navbar
+        navbar: navbar,
+        logo:true
     })
 })
 
